6f32e514178d923af8c40053f24ffdb8abfe38c4
Alex Zurek <zureka@mail.gvsu.edu>
2019-12-12 16:02:15 -0500

    Completes part 2 of Day 1

--- src/day_1.clj
+++ src/day_1.clj
@@ -4,12 +4,28 @@
   [mass]
   (- (quot mass 3) 2))
 
+(defn calc-mass
+  [mass]
+  (if (<= mass 0)
+    0
+    (+ mass (calc-mass (fuel-for-mass mass)))))
+
+(defn fuel-for-mass-of-fuel
+  [mass]
+  (let [fuel-mass (fuel-for-mass mass)]
+    (calc-mass fuel-mass)))
+
 (defn read-file
   [filename]
   (with-open [rdr (clojure.java.io/reader (str "input/" filename))]
     (reduce conj [] (line-seq rdr))))
 
+(defn sum-input
+  [func]
+  (reduce + 0 (map #(func (Integer. %)) (read-file "day_1_input.txt"))))
+
 (defn -main []
-  (print "The answer is"
-         (reduce + 0 (map #(fuel-for-mass (Integer. %)) (read-file "day_1_input.txt")))))
+  (do
+    (println "The answer for part 1 is:" (sum-input fuel-for-mass))
+    (println "The answer for part 2 is:" (sum-input fuel-for-mass-of-fuel))))
 
