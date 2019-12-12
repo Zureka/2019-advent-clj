1ba8d8d6312efb061931f2f2d3ef43b2a3cfdbd9
Alex Zurek <zureka@mail.gvsu.edu>
2019-12-12 16:25:29 -0500

    Adds tests for part 2 of day 1

--- test/day_1_test.clj
+++ test/day_1_test.clj
@@ -8,3 +8,8 @@
   (is (= 654 (fuel-for-mass 1969)))
   (is (= 33583 (fuel-for-mass 100756))))
 
+(deftest test-fuel-for-mass-of-fuel
+  (is (= 2 (fuel-for-mass-of-fuel 14)))
+  (is (= 966 (fuel-for-mass-of-fuel 1969)))
+  (is (= 50346 (fuel-for-mass-of-fuel 100756))))
+
